8cd3d45c7498bec4b001e27d99edc1d6421e20e3
borugaddaebenezer <chantiborugadda5@gmail..com>
2025-07-08 18:46:33 +0530

    logo correction

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,8 +7,9 @@ import useLogout from "../hooks/useLogout";
 const Navbar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
+  
   const isChatPage = location.pathname?.startsWith("/chat");
-
+  const isHomePage = window.location.href;
 
   const { logoutMutation } = useLogout();
 
@@ -16,9 +17,21 @@ const Navbar = () => {
     <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-end w-full">
+       {
+        isHomePage && !isChatPage &&(
+             <div className="p-5 border-b border-base-300 block lg:hidden">
+        <Link to="/" className="flex items-center gap-2.5">
+          <ShipWheelIcon className="size-9 text-primary" />
+          <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
+            Voxta
+          </span>
+        </Link>
+      </div>
+        )
+       }
           {/* LOGO - ONLY IN THE CHAT PAGE */}
           {isChatPage && (
-            <div className="pl-5">
+            <div className="pl-5 block ">
               <Link to="/" className="flex items-center gap-2.5">
                 <ShipWheelIcon className="size-9 text-primary" />
                 <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
