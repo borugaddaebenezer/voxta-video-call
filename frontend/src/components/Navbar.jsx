b54e9c17183782307e33d797b7d4c18fe325dd5e
borugaddaebenezer <chantiborugadda5@gmail..com>
2025-07-08 19:11:18 +0530

    reset to initial

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
        } */}
           {/* LOGO - ONLY IN THE CHAT PAGE */}
           {isChatPage && (
-            <div className="pl-5 block ">
+            <div className="pl-5">
               <Link to="/" className="flex items-center gap-2.5">
                 <ShipWheelIcon className="size-9 text-primary" />
                 <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
